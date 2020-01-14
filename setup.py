7983e488c0242c4f6f23417d284c3ca0545dc375
con-f-use <con-f-use@gmx.net>
2020-01-14 19:38:36 +0100

    setup.py cleanup

--- setup.py
+++ setup.py
@@ -1,47 +1,33 @@
 #!/usr/bin/env python
-import re, time, datetime
+import re
 from setuptools import setup
 
 try:
-    with open('README.md') as fh:
+    with open("README.md") as fh:
         README = fh.read()
     description = next(s for s in README.splitlines()[2:] if re.match(r"^\w", s))
 except (IOError, OSError):
-    README = description = ''
-
-
-def loc_vers(*a, **kw):
-    import dateutil.parser
-
-    scm = a[0]
-    mems = '{distance=}, {dirty=}, {branch=}, {node=}, {tag=}, {time'
-    for mem in mems.split('=}, '):
-        mem = mem.strip('{')
-        print(f'{mem} {type(scm.__dict__[mem])}= {scm.__dict__[mem]}')
-    print(scm.time.tzname())
-    print("\n")
-    return ""
+    README = description = ""
 
 
 setup(
-    name='spect',
-    url='https://github.com/con-f-use/spect',
-    author='con-f-use',
-    author_email='con-f-use@gmx.net',
+    name="spect",
+    url="https://github.com/con-f-use/spect",
+    author="con-f-use",
+    author_email="con-f-use@gmx.net",
     description=description,
     long_description=README,
-    long_description_content_type='text/markdown',
-    use_scm_version=dict(local_scheme=loc_vers),
-    setup_requires=["setuptools_scm", "python-dateutil"],
+    long_description_content_type="text/markdown",
+    use_scm_version=dict(local_scheme=lambda _: ""),
+    setup_requires=["setuptools_scm"],
     include_package_data=True,
     zip_safe=True,
     options={"bdist_wheel": {"universal": True}},
     package_dir={"": "src"},
     packages=["spect"],
     classifiers=[
-        'Intended Audience :: Developers',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Topic :: Desktop Environment',
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
     ],
 )
